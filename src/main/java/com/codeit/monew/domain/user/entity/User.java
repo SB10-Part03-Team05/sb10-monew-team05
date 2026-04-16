b498caf1e394152c21eac2d1d6c41aa0d427d429
plzslp <gyutann1171@gmail.com>
2026-04-16 15:19:03 +0900

    feat: BaseEntity 클래스 작성
    --- 본문 (선택 사항, 상세 설명이 필요한 경우) ---
    - BaseEntity: ID와 createdAt을 공통으로 관리하는 추상 클래스 구현
    - BaseUpdatableEntity: BaseEntity를 상속받으며 updatedAt 필드 추가
    - User: BaseUpdatableEntity를 상속받도록 변경
    
    --- 꼬리말 (선택 사항, 이슈 번호 연결) ---

--- src/main/java/com/codeit/monew/domain/user/entity/User.java
+++ src/main/java/com/codeit/monew/domain/user/entity/User.java
@@ -1,38 +1,19 @@
 package com.codeit.monew.domain.user.entity;
 
+import com.codeit.monew.global.common.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.Instant;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @Table(name = "users")
-public class User {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
-
-  @CreatedDate
-  @Column(nullable = false, updatable = false)
-  private Instant createdAt;
-
-  @LastModifiedDate
-  private Instant updatedAt;
+public class User extends BaseUpdatableEntity {
 
   @Column(nullable = false, unique = true)
   private String email;
@@ -43,6 +24,7 @@ public class User {
   @Column(nullable = false)
   private String password;
 
+  @Column
   private Instant deletedAt;
 
   public User(String email, String nickname, String password) {
