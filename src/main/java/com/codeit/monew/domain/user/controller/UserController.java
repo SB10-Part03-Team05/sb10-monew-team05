c8fa24deef54994770a5a175832b6768453d5c81
plzslp <gyutann1171@gmail.com>
2026-04-16 16:22:08 +0900

    refactor: RequestMapping의 엔드포인트 수정

--- src/main/java/com/codeit/monew/domain/user/controller/UserController.java
+++ src/main/java/com/codeit/monew/domain/user/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
