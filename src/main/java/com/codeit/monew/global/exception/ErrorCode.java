381e3ae354fcaa82c427b4fac04f8efdcf51b59a
JungH200000 <jhpaxk0604@gmail.com>
2026-04-16 13:04:23 +0900

    feat: 뉴스 기사 정보 조회 실패 전용 코드 `ARTICLE_NOT_FOUND` 추가

--- src/main/java/com/codeit/monew/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/monew/global/exception/ErrorCode.java
@@ -4,21 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    // User 관련 에러 코드
+  // User 관련 에러 코드
 
-    // Article 관련 에러 코드
+  // Article 관련 에러 코드
+  ARTICLE_NOT_FOUND("뉴스 기사 정보를 찾을 수 없습니다."),
 
-    // Interest 관련 에러 코드
+  // Interest 관련 에러 코드
 
-    // Comment 관련 에러 코드
+  // Comment 관련 에러 코드
 
-    // Server 에러 코드
-    INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
-    INVALID_REQUEST("잘못된 요청입니다.");
+  // Server 에러 코드
+  INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
+  INVALID_REQUEST("잘못된 요청입니다.");
 
-    private final String message;
+  private final String message;
 
-    ErrorCode(String message) {
-        this.message = message;
-    }
+  ErrorCode(String message) {
+    this.message = message;
+  }
 }
