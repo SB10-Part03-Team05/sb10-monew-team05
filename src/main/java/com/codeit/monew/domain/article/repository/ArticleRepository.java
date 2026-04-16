4a0a4a2a2b4410e0b668b14aad8d2a9f12275bac
JungH200000 <jhpaxk0604@gmail.com>
2026-04-16 21:40:32 +0900

    feat: 뉴스 기사 출처 목록 조회 repository 메서드 추가

--- src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
@@ -1,11 +1,17 @@
 package com.codeit.monew.domain.article.repository;
 
+import com.codeit.monew.domain.article.ArticleSource;
 import com.codeit.monew.domain.article.entity.Article;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
 
   Optional<Article> findByIdAndDeletedAtIsNull(UUID id);
+
+  @Query("SELECT DISTINCT source FROM Article ORDER BY source ASC ")
+  List<ArticleSource> findDistinctSource();
 }
