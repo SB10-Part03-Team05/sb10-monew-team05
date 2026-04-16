5f23e2cdfcd3285644405e19bc17b837caaeff30
plzslp <gyutann1171@gmail.com>
2026-04-16 15:14:11 +0900

    feat: 사용자 등록 Service/Controller 구현
    --- 본문 (선택 사항, 상세 설명이 필요한 경우) ---
    - UserService: 회원가입 로직 작성
    - UserController: 사용자 등록 엔드포인트 추가
    - ErrorCode: USER_NOT_FOUND, DUPLICATE_EMAIL 추가
    - UserRegisterRequest: 비밀번호에 패턴 유효성 검증 추가
    
    --- 꼬리말 (선택 사항, 이슈 번호 연결) ---
    resolved: #20

---
+++ src/main/java/com/codeit/monew/domain/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.codeit.monew.domain.user.controller;
+
+import com.codeit.monew.domain.user.dto.UserDto;
+import com.codeit.monew.domain.user.dto.UserRegisterRequest;
+import com.codeit.monew.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<UserDto> register(
+      @Valid @RequestBody UserRegisterRequest request
+  ) {
+    log.info("유저 회원가입 요청: email={}, nickname={}", request.email(), request.nickname());
+    UserDto dto = userService.register(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+  }
+}
