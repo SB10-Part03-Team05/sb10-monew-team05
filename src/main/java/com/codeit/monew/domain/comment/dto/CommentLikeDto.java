0c568a137ad9f92ce8cb71a942bd1f6304b1892a
parkngg <millco0803com@gmail.com>
2026-04-16 11:46:58 +0900

    feat: 댓글 DTO 추가

---
+++ src/main/java/com/codeit/monew/domain/comment/dto/CommentLikeDto.java
@@ -0,0 +1,18 @@
+package com.codeit.monew.domain.comment.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CommentLikeDto(
+    UUID id,
+    UUID likedBy,
+    Instant createdAt,
+    UUID commentId,
+    UUID articleId,
+    UUID commentUserId,
+    String commentUserNickname,
+    String commentContent,
+    Long commentLikeCount,
+    Instant commentCreatedAt
+) {
+}
