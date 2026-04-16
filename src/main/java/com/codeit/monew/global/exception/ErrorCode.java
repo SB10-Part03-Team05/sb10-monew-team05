0c18a15c2e8b64912cab1f92880c54ae3cf788ae
JungH200000 <jhpaxk0604@gmail.com>
2026-04-16 13:35:13 +0900

    feat: 사용자 정보 조회 실패 전용 코드 `USER_NOT_FOUND` 추가

--- src/main/java/com/codeit/monew/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/monew/global/exception/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
   // User 관련 에러 코드
+  USER_NOT_FOUND("사용자 정보를 찾을 수 없습니다."),
 
   // Article 관련 에러 코드
   ARTICLE_NOT_FOUND("뉴스 기사 정보를 찾을 수 없습니다."),
