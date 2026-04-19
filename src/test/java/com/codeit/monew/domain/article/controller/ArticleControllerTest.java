1e59fddf1bb2eee4d42ddc47d438748cf475785d
JungH200000 <jhpaxk0604@gmail.com>
2026-04-19 19:34:03 +0900

    test: 뉴스 기사 목록 조회 Controller 테스트 케이스 작성

--- src/test/java/com/codeit/monew/domain/article/controller/ArticleControllerTest.java
+++ src/test/java/com/codeit/monew/domain/article/controller/ArticleControllerTest.java
@@ -1,14 +1,21 @@
 package com.codeit.monew.domain.article.controller;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
+import com.codeit.monew.domain.article.dto.request.ArticleSearchRequest;
 import com.codeit.monew.domain.article.dto.response.ArticleDto;
 import com.codeit.monew.domain.article.ArticleSource;
+import com.codeit.monew.domain.article.dto.response.CursorPageResponseArticleDto;
+import com.codeit.monew.domain.article.entity.type.ArticleDirection;
+import com.codeit.monew.domain.article.entity.type.ArticleOrderBy;
 import com.codeit.monew.domain.article.service.ArticleService;
 import com.codeit.monew.global.exception.ErrorCode;
 import com.codeit.monew.global.exception.GlobalExceptionHandler;
 import com.codeit.monew.global.exception.article.ArticleNotFoundException;
+import com.codeit.monew.global.exception.interest.InterestNotFoundException;
 import com.codeit.monew.global.exception.user.UserNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
@@ -23,6 +30,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -44,8 +52,8 @@ class ArticleControllerTest {
   private ArticleService articleService;
 
   private ArticleDto createArticleDto(UUID articleId, ArticleSource source, String sourceUrl,
-      String title, Instant publishDate,
-      String summary, long commentCount, long viewCount, boolean viewedByMe) {
+      String title, Instant publishDate, String summary, long commentCount, long viewCount,
+      boolean viewedByMe) {
     if (articleId == null) {
       articleId = UUID.randomUUID();
     }
@@ -138,6 +146,148 @@ class ArticleControllerTest {
           .andExpect(jsonPath("$[0]").value(ArticleSource.HANKYUNG.toString()))
           .andExpect(jsonPath("$[1]").value(ArticleSource.NAVER.toString()))
           .andExpect(jsonPath("$[2]").value(ArticleSource.YEONHAP.toString()));
+    }
+  }
+
+  @Nested
+  @DisplayName("뉴스 기사 목록 조회 API 테스트")
+  class search {
+
+    @Test
+    @DisplayName("뉴스 기사 목록 조회하면 200 상태코드와 커서 페이지네이션이 적용된 뉴스 기사 목록이 반환된다.")
+    void success_search_article_list() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+      ArticleDto articleDto1 = createArticleDto(null, ArticleSource.NAVER, "https://naver.com",
+          "testTitle1", Instant.now(), "testSummary1", 5, 6, true);
+      ArticleDto articleDto2 = createArticleDto(null, ArticleSource.YEONHAP, "https://yeonhap.com",
+          "testTitle2", Instant.parse("2026-04-17T09:12:15Z"), "testSummary2", 3, 7, false);
+
+      CursorPageResponseArticleDto response = new CursorPageResponseArticleDto(
+          List.of(articleDto1, articleDto2),
+          "2026-04-17T09:12:15Z",
+          Instant.parse("2026-04-17T09:12:15Z"),
+          2,
+          2,
+          false
+      );
+
+      given(articleService.search(any(ArticleSearchRequest.class), eq(requestUserId))).willReturn(
+          response);
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("keyword", "Spring Boot")
+              .param("orderBy", ArticleOrderBy.publishDate.toString())
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "2")
+              .header("Monew-Request-User-ID", requestUserId.toString()))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.content[0].id").value(articleDto1.id().toString()))
+          .andExpect(jsonPath("$.content[1].id").value(articleDto2.id().toString()))
+          .andExpect(jsonPath("$.size").value(2));
+    }
+
+    @Test
+    @DisplayName("필수 파라미터(orderBy/direction/limit) 누락 시 400 상태코드와 MethodArgumentNotValidException 예외 발생")
+    void fail_search_article_list_when_required_parameter_is_not_valid() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "5")
+              .header("Monew-Request-User-ID", requestUserId))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+          .andExpect(jsonPath("$.status").value(400))
+          .andExpect(jsonPath("$.exceptionType").value(
+              MethodArgumentNotValidException.class.getSimpleName()));
+    }
+
+    @Test
+    @DisplayName("keyword가 공백일 경우 400 상태코드와 MethodArgumentNotValidException 예외 발생")
+    void fail_search_article_list_when_keyword_is_not_blank() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("keyword", "")
+              .param("orderBy", ArticleOrderBy.publishDate.toString())
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "5")
+              .header("Monew-Request-User-ID", requestUserId))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+          .andExpect(jsonPath("$.status").value(400))
+          .andExpect(jsonPath("$.exceptionType").value(
+              MethodArgumentNotValidException.class.getSimpleName()));
+    }
+
+    @Test
+    @DisplayName("limit이 1미만일 경우 400 상태코드와 MethodArgumentNotValidException 예외 발생")
+    void fail_search_article_list_when_limit_is_less_than_1() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("orderBy", ArticleOrderBy.publishDate.toString())
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "-1")
+              .header("Monew-Request-User-ID", requestUserId))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+          .andExpect(jsonPath("$.status").value(400))
+          .andExpect(jsonPath("$.exceptionType").value(
+              MethodArgumentNotValidException.class.getSimpleName()));
+    }
+
+    @Test
+    @DisplayName("사용자 ID를 찾을 수 없을 경우 404 상태코드와 UserNotFoundException 예외 발생")
+    void fail_search_article_list_when_userId_not_found() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+
+      given(articleService.search(any(ArticleSearchRequest.class), eq(requestUserId))).willThrow(
+          new UserNotFoundException(requestUserId));
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("orderBy", ArticleOrderBy.publishDate.toString())
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "2")
+              .header("Monew-Request-User-ID", requestUserId))
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.code").value(ErrorCode.USER_NOT_FOUND.toString()))
+          .andExpect(jsonPath("$.status").value(404))
+          .andExpect(
+              jsonPath("$.exceptionType").value(UserNotFoundException.class.getSimpleName()));
+    }
+
+    @Test
+    @DisplayName("관심사 ID를 찾을 수 없을 경우 404 상태코드와 InterestNotFoundException 예외 발생")
+    void fail_search_article_list_when_interestId_not_found() throws Exception {
+      // given(준비)
+      UUID requestUserId = UUID.randomUUID();
+      UUID interestId = UUID.randomUUID();
+
+      given(articleService.search(any(ArticleSearchRequest.class), eq(requestUserId))).willThrow(
+          new InterestNotFoundException(interestId));
+
+      // when(시작), then(검증)
+      mockMvc.perform(get("/api/articles")
+              .param("orderBy", ArticleOrderBy.publishDate.toString())
+              .param("direction", ArticleDirection.DESC.toString())
+              .param("limit", "2")
+              .header("Monew-Request-User-ID", requestUserId))
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.code").value(ErrorCode.INTEREST_NOT_FOUND.toString()))
+          .andExpect(jsonPath("$.status").value(404))
+          .andExpect(
+              jsonPath("$.exceptionType").value(InterestNotFoundException.class.getSimpleName()));
     }
   }
 }
