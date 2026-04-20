7f81ede75a2316a24fd40af958817549cc43aa40
박린 <lynnpark2003@ehwa.ac.kr>
2026-04-20 11:04:01 +0900

    refactor: SubscriptionDto 필드 반환 타입 수정

---
+++ src/test/java/com/codeit/monew/domain/interest/controller/InterestControllerTest.java
@@ -0,0 +1,4 @@
+package com.codeit.monew.domain.interest.controller;
+
+public class InterestControllerTest {
+}
---
+++ src/test/java/com/codeit/monew/domain/interest/service/InterestServiceTest.java
@@ -0,0 +1,4 @@
+package com.codeit.monew.domain.interest.service;
+
+public class InterestServiceTest {
+}
