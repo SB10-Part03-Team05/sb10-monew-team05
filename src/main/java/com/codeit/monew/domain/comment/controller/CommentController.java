911ee0c701f744989ca6f7d808e016d9c0e00d66
parkngg <millco0803com@gmail.com>
2026-04-20 11:22:34 +0900

    chore: Swagger API 문서 수정

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -36,7 +36,7 @@ public class CommentController {
   @Operation(summary = "댓글 등록", description = "새로운 댓글을 등록합니다.")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "등록 성공"),
-      @ApiResponse(responseCode = "403", description = "수정 권한 없음"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청 (입력값 검증 실패)"),
       @ApiResponse(responseCode = "500", description = "서버 내부 오류")
   })
   @PostMapping
@@ -59,7 +59,8 @@ public class CommentController {
   @Operation(summary = "댓글 정보 수정", description = "댓글의 내용을 수정합니다.")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "수정 성공"),
-      @ApiResponse(responseCode = "400", description = "잘못된 요청 (입력값 검증 실패 / 권한 없음)"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청 (입력값 검증 실패)"),
+      @ApiResponse(responseCode = "403", description = "수정 권한 없음"),
       @ApiResponse(responseCode = "404", description = "댓글 정보 없음"),
       @ApiResponse(responseCode = "500", description = "서버 내부 오류")
   })
