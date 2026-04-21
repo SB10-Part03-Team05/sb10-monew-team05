b7e12e9e2622ee225d9852d9733c54e5d5804e8c
JungH200000 <jhpaxk0604@gmail.com>
2026-04-21 17:16:20 +0900

    feat: 뉴스 기사 물리 삭제 Repository 메서드와 JPQL 구현

--- src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, UUID>, ArticleQueryRepository {
 
@@ -18,4 +20,11 @@ public interface ArticleRepository extends JpaRepository<Article, UUID>, Article
   // 논리 삭제된 기사 포함
   @Query(value = "SELECT EXISTS (SELECT 1 FROM articles WHERE source_url = :sourceUrl)", nativeQuery = true)
   boolean existsBySourceUrl(String sourceUrl);
+
+  // 뉴스 기사 물리 삭제
+  // `@SQLRestriction`로 조회 시 deletedAt이 null인 데이터만 가져오도록 필터링하기 때문에
+  // `void` 가 아닌 `int` 로 값을 반환해 id가 존재하는 기사인지 검증도 같이함
+  @Modifying // 조회 쿼리가 아님을 JPA에게 명시
+  @Query("DELETE FROM Article AS a WHERE a.id = :articleId")
+  int hardDelete(@Param("articleId") UUID articleId);
 }
