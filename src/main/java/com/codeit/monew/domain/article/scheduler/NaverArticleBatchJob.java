bea181ded9c1c998ffd9af7141d1f34cf5d0d32c
ParkSungGuk <cdjsdj1902@gmail.com>
2026-04-24 01:26:44 +0900

    fix: sleep 메서드 접근 제어자 package(생략)으로 변경

--- src/main/java/com/codeit/monew/domain/article/scheduler/NaverArticleBatchJob.java
+++ src/main/java/com/codeit/monew/domain/article/scheduler/NaverArticleBatchJob.java
@@ -95,7 +95,7 @@ public class NaverArticleBatchJob {
     return result;
   }
 
-  protected void sleep(long millis) {
+  void sleep(long millis) {
     try {
       Thread.sleep(millis);
     } catch (InterruptedException e) {
