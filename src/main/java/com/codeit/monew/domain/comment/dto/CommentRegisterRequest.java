bc13a6bae4caa47233b92007d6a2c4160834ec4c
parkngg <millco0803com@gmail.com>
2026-04-29 14:57:55 +0900

    docs: CommentRegisterRequest 스웨거 maxLength 속성 명시

--- src/main/java/com/codeit/monew/domain/comment/dto/CommentRegisterRequest.java
+++ src/main/java/com/codeit/monew/domain/comment/dto/CommentRegisterRequest.java
@@ -17,7 +17,7 @@ public record CommentRegisterRequest(
     @NotNull(message = "사용자 ID는 필수입니다.")
     UUID userId,
 
-    @Schema(description = "댓글 내용")
+    @Schema(description = "댓글 내용", maxLength = 500)
     @NotBlank(message = "댓글 내용은 필수입니다.")
     @Size(min = 1, max = 500, message = "댓글 내용은 1자 이상 500자 이하여야 합니다.")
     String content
