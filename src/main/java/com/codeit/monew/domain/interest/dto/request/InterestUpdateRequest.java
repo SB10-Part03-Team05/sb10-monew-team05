2f3ba9770b3e62c5f9871adfc0aae2caeba66fe0
박린 <lynnpark2003@ehwa.ac.kr>
2026-04-29 14:24:47 +0900

    docs: 관심사 DTO Swagger API 문서화 작업

--- src/main/java/com/codeit/monew/domain/interest/dto/request/InterestUpdateRequest.java
+++ src/main/java/com/codeit/monew/domain/interest/dto/request/InterestUpdateRequest.java
@@ -1,11 +1,14 @@
 package com.codeit.monew.domain.interest.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
+@Schema(description = "수정할 관심사 정보")
 public record InterestUpdateRequest(
+    @Schema(description = "수정 키워드 목록", maxLength = 10)
     @NotEmpty
     @Size(max = 10)
     List<@NotBlank String> keywords
