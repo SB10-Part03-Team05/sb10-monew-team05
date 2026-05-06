2437e28f56cd815309a7507196d7db2b1f23921b
박린 <lynnpark2003@ehwa.ac.kr>
2026-05-06 15:17:15 +0900

    refactor: KeyGenerator 방식으로 수정

--- src/main/java/com/codeit/monew/domain/article/service/ArticleService.java
+++ src/main/java/com/codeit/monew/domain/article/service/ArticleService.java
@@ -80,7 +80,7 @@ public class ArticleService {
   }
 
   // 뉴스 기사 목록 조회(커서 페이지네이션)
-  @Cacheable(value = "articleList", key = "#request.orderBy + '_' + #request.direction + '_' + #request.cursor + '_' + #request.after + '_' + #request.keyword + '_' + #request.sourceIn + '_' + #request.publishDateFrom + '_' + #request.publishDateTo + '_' + #request.limit + '_' + #request.interestId + '_' + #requestUserId")
+  @Cacheable(value = "articleList", keyGenerator = "articleListKeyGenerator")
   @Transactional(readOnly = true)
   public CursorPageResponseArticleDto search(ArticleSearchRequest request, UUID requestUserId) {
     log.debug(
